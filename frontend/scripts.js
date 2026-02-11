fb7f3426a44c773cef4c8f890aea3392134364c9
peco220807 <saparbai.s07@icloud.com>
2026-02-12 00:25:34 +0500

    only FrontEnd

---
+++ frontend/scripts.js
@@ -0,0 +1,11 @@
+﻿document.querySelectorAll('.category-card').forEach(card => {
+  card.addEventListener('click', () => {
+    alert('You selected: ' + card.querySelector('h2').innerText);
+  });
+});
+
+document.querySelectorAll('.price-filters button').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Filtering laptops under: ' + button.innerText);
+  });
+});
